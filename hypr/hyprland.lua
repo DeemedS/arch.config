9e66361f29b45e2df91e5de80a41016a9ad48f96
seedot <guianalankem@gmail.com>
2026-05-14 06:20:37 +0800

    Initial Config

---
+++ hypr/hyprland.lua
@@ -0,0 +1,42 @@
+-- AUTOSTART
+hl.on("hyprland.start", function () 
+  hl.exec_cmd("waybar")
+end)
+
+-- APPEARANCE
+
+-- KEYBINDS
+hl.bind("SUPER + T", hl.dsp.exec_cmd("kitty") )
+hl.bind("SUPER + E", hl.dsp.exec_cmd("dolpin") )
+hl.bind("SUPER + P", hl.dsp.exec_cmd("killall waybar; waybar") )
+hl.bind("SUPER + B", hl.dsp.exec_cmd("brave --password-store=basic") )
+hl.bind("SUPER + Q", hl.dsp.window.close())
+
+for i = 1, 9 do
+ local numberkey = {10,11,12,13,14,15,16,17,18,19}
+ hl.bind("SUPER + code:"..numberkey[i], hl.dsp.focus({ workspace = i}) )
+end
+
+--move focus
+hl.bind("CTRL + Left",  hl.dsp.focus({direction = "l"}))
+hl.bind("CTRL + Right", hl.dsp.focus({direction = "r"}))
+hl.bind("CTRL + Up",    hl.dsp.focus({direction = "u"}))
+hl.bind("CTRL + Down",  hl.dsp.focus({direction = "d"}))
+
+--move window
+hl.bind("SUPER + CTRL + Left",  hl.dsp.window.move({direction = "l"}))
+hl.bind("SUPER + CTRL + Right",  hl.dsp.window.move({direction = "r"}))
+hl.bind("SUPER + CTRL + Up",  hl.dsp.window.move({direction = "u"}))
+hl.bind("SUPER + CTRL + Down",  hl.dsp.window.move({direction = "d"}))
+
+
+
+-- WINDOW RULES
+hl.window_rule({
+  name = "audio-control",
+  match = {
+    class = "org.pulseaudio.pavucontrol"
+  },
+  float = true,
+  size = { 900, 600 }
+})
